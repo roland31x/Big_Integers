fef5c90818a2acc9ee0c92597d294317936c00b5
roland31x <roland31x@gmail.com>
2022-11-24 01:06:36 +0200

    Add project files.

---
+++ Big_Integers/Program.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.Win32;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Big_Integers
+{
+    public struct NumarMare
+    {
+        public char[] cifre; // atribut 1 : cifrele ca caractere
+        public int[] cifreint; // atribut 2 : cifrele ca int
+        public int[] cifrereversed; // atribut 3 : cifrele ca int in ordine inversa pentru efectuarea calculelor
+        public NumarMare(string s)
+        {
+            cifre = s.ToCharArray();
+            cifreint = new int[cifre.Length]; 
+            cifrereversed= new int[cifre.Length];
+            for (int i = 0, j = cifre.Length - 1 ; i < cifre.Length; i++, j--)
+            {
+                cifrereversed[i] = Convert.ToInt32(cifre[j].ToString());
+            }
+            for(int i = 0; i < cifre.Length; i++)
+            {
+                cifreint[i] = Convert.ToInt32(cifre[i].ToString());
+            }
+            //Console.WriteLine("c:");
+            //for (int i = 0; i < cifre.Length; i++)
+            //{
+            //    Console.Write(cifre[i]);
+            //}
+            //Console.WriteLine("i:");
+            //for (int i = 0; i < cifre.Length; i++)
+            //{
+            //    Console.Write(cifreint[i]);
+            //}
+            //Console.WriteLine("r:");
+            //for (int i = 0; i < cifre.Length; i++)
+            //{
+            //    Console.Write(cifrereversed[i]);
+            //}
+
+        }
+        public static NumarMare operator +(NumarMare left, NumarMare right)
+        {
+            int additionlength;
+            NumarMare aux;
+            bool rightsmaller = true;
+            if (left < right)
+            {
+                aux = left;
+                left = right;
+                right = aux;
+            }
+            additionlength = left.cifre.Length;
+            additionlength++;
+            StringBuilder str = new StringBuilder();
+            while (additionlength > 0)
+            {
+                str.Append("0");
+                additionlength--;
+            }
+            NumarMare add = new NumarMare(str.ToString());
+            for (int i = 0; i < add.cifre.Length - 1; i++)
+            {
+                if(i >= right.cifre.Length && rightsmaller)
+                {
+                    add.cifrereversed[i] += left.cifrereversed[i];
+                    if (add.cifrereversed[i] > 9)
+                    {
+                        add.cifrereversed[i] = add.cifrereversed[i] % 10;
+                        add.cifrereversed[i + 1] += 1;
+                    }
+                    continue;
+                }
+                add.cifrereversed[i] += left.cifrereversed[i] + right.cifrereversed[i];
+                if (add.cifrereversed[i] > 9)
+                {
+                    add.cifrereversed[i] = add.cifrereversed[i] % 10;
+                    add.cifrereversed[i + 1] += 1;
+                }
+            }
+            str = new StringBuilder();
+            foreach(int i in add.cifrereversed)
+            {
+                str.Append(i);
+            }
+            add = new NumarMare(str.ToString());
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write(add.cifreint[i]);
+            //}
+            //Console.WriteLine(str.ToString());
+            add = add.Normalize(); 
+            return add;
+        }
+        //public static NumarMare operator -(NumarMare left, NumarMare right)
+        //{
+
+        //}
+        //public static NumarMare operator *(NumarMare left, NumarMare right)
+        //{
+
+        //}
+        //public static NumarMare operator /(NumarMare left, NumarMare right)
+        //{
+
+        //}
+        //public static NumarMare Pow(NumarMare base, int exp)
+        //{
+
+        //}
+
+
+        public static bool operator >(NumarMare left, NumarMare right)
+        {
+            if (left.cifre.Length > right.cifre.Length)
+            {
+                return true;
+            }
+            else if(left.cifre.Length < right.cifre.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = left.cifre.Length - 1 ; i >= 0 ; i--)
+                {
+                    if (left.cifre[i] > right.cifre[i])
+                    {
+                        return true;
+                    }
+                    else if (left.cifre[i] < right.cifre[i])
+                    {
+                        return false;
+                    }
+                    else continue;
+                }
+            }
+            return false;
+        }
+        public static bool operator <(NumarMare left, NumarMare right)
+        {
+            if (left > right) return false;
+            else return true;
+        }
+        public static bool operator >=(NumarMare left, NumarMare right)
+        {
+            if (left.cifre.Length > right.cifre.Length)
+            {
+                return true;
+            }
+            else if (left.cifre.Length < right.cifre.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = left.cifre.Length - 1; i >= 0; i--)
+                {
+                    if (left.cifre[i] > right.cifre[i])
+                    {
+                        return true;
+                    }
+                    else if (left.cifre[i] < right.cifre[i])
+                    {
+                        return false;
+                    }
+                    else continue;
+                }
+            }
+            return true;
+        }
+        public static bool operator <=(NumarMare left, NumarMare right)
+        {
+            if (left > right) return false;
+            else return true;
+        }
+        public static bool operator ==(NumarMare left,NumarMare right)
+        {
+            if (left >= right && right >= left) return true;
+            else return false;
+        }
+        public static bool operator !=(NumarMare left, NumarMare right)
+        {
+            if (left >= right && right >= left) return false;
+            else return true;
+        }
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                NumarMare p = (NumarMare)obj;
+                if (this == p)
+                {
+                    return true;
+                }
+                else return false;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public NumarMare Normalize()
+        {
+            string str = "";
+            bool started = false;
+            for(int i = 0; i < cifre.Length; i++)
+            {
+                if (cifrereversed[i] == 0 && !started)
+                {
+                    continue;
+                }
+                if (cifrereversed[i] != 0)
+                {
+                    started = true;
+                }
+                str = str + cifrereversed[i].ToString();
+            }
+            NumarMare rev = new NumarMare(str);
+            return rev;
+        } 
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            for(int i = 0; i < cifre.Length; i++)
+            {
+                str.Append(Convert.ToString(cifre[i]));
+            }
+            string s = str.ToString();
+            return s;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumarMare a = new NumarMare("1013");
+
+            //for(int i = 0; i < 4; i++)
+            //{
+            //    Console.Write(a.cifre[i]);
+            //}
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.Write(a.cifreint[i]);
+            //}
+
+            NumarMare b = new NumarMare("1013");
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(a > b);
+            Console.WriteLine(a >= b);
+            Console.WriteLine(a.Equals(new NumarMare("1013")));
+            Console.WriteLine(a + b);
+            
+        }
+    }
+}
